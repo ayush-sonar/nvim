c2fe0d8d5391caf220266613f4e5f228865bcb51
Ayush Sonar <ayusht1d@gmail.com>
2025-04-12 02:24:50 +0530

    intial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+-- Set leader key (used in many custom keybindings)
+vim.g.mapleader = " "
+
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Enable mouse support
+vim.opt.mouse = "a"
+
+-- Basic key mappings (quicker save/quit)
+vim.keymap.set("n", "<leader>w", ":w<CR>")  -- Save with space+w
+vim.keymap.set("n", "<leader>q", ":q<CR>")  -- Quit with space+q
+
+vim.keymap.set("n", "<leader>rn",":set relativenumber!<CR>", { noremap = true }) 
+
+
