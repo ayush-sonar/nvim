1cd6bfcd83c348e80d7303404bcd8ec0dd9cfd7f
Ayush Sonar <ayusht1d@gmail.com>
2025-06-10 00:51:39 +0530

    update

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -7,7 +7,7 @@ return {
 {"williamboman/mason-lspconfig.nvim",
 	config = function()
 		require("mason-lspconfig").setup({
-		ensure_installed = {"lua_ls","pylsp"}}
+		ensure_installed = {"lua_ls","pylsp","ts_ls"}}
 		)
 	end
 
@@ -16,8 +16,11 @@ return {
 	config = function()
 		local lspconfig = require("lspconfig")
 		lspconfig.lua_ls.setup({})
+        lspconfig.ts_ls.setup({})
+        lspconfig.pylsp.setup({})
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 	end
 }
 }
